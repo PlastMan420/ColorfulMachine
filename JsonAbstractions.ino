fd7b413c32ee9c742af30fa931bae23abbebe008
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-06-25 20:10:50 +0200

    [CHORE] code refactors, remove color enum. change how json is generated

--- JsonAbstractions.ino
+++ JsonAbstractions.ino
@@ -3,42 +3,43 @@
 #include <ArduinoJson.h>
 #include "JsonAbstractions.h"
 
-enum ColorSet {
-  RED,
-  YELLOW,
-  BLUE,
-  GREEN,
-  ORANGE,
-  PURPLE,
-
-};
-
-const byte staticJsonSize = 32;
 //const char *filename = "/config.txt";
 
 void SdCardInit() {
-  if(debug) Serial.print("Initializing SD card...");
-  
+  #if DEBUG == true
+    Serial.print("Initializing SD card...");
+  #endif
   SD.begin(CS);
 
+}
+
+void ReadConfig() {
+  
 }
 
 void UpdateConfig(struct Config *config, const char *filename) {
   // drop parameter translates into accelestepper "location".
   // color is a HEX representation of RGB.
+  const byte staticJsonSize = 32;
+
   File file = SD.open(filename);
 
   StaticJsonDocument<staticJsonSize> conf;
   // Deserialize the JSON document
   DeserializationError error = deserializeJson(conf, file);
   if (error)
-    Serial.println(F("Failed to read file."));
+    {
+      Serial.println(F("Failed to read file."));
+      return;
+    }
 
-  conf["color"] = config->color;
-  conf["drop"] = config->drop;
+  conf[config->drop] = config->color;
 
   serializeJson(conf, file);
 
   file.close();
 }
 
+void UpdateLog() {
+  
+}
