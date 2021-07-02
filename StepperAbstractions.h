4d56e2a789485f0c4f28a37bb976b2cb4ee22a36
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-07-02 14:06:57 +0200

    [FEAT] output carousel used takes 850 stepper steps per slot.

--- StepperAbstractions.h
+++ StepperAbstractions.h
@@ -1,5 +1,6 @@
 #pragma once
 
+const PROGMEM byte STEPPER_INTERFACE_TYPE = 4;
 const PROGMEM byte STEPPER_IN1 = 22;
 const PROGMEM byte STEPPER_IN2 = 24;
 const PROGMEM byte STEPPER_IN3 = 26;
--- StepperAbstractions.ino
+++ StepperAbstractions.ino
@@ -1,13 +1,15 @@
 #include <AccelStepper.h>
 #include "StepperAbstractions.h"
 
+const PROGMEM int STEPS = 850;
+
 // choose '4' in the first parameter for ULN2003AN controlling or similar.
 // IN1, pwmIN2, pwmIN3, pwmIN4.
-AccelStepper carousel(4, STEPPER_IN1, STEPPER_IN2, STEPPER_IN3, STEPPER_IN4);
+AccelStepper carousel(STEPPER_INTERFACE_TYPE, STEPPER_IN1, STEPPER_IN2, STEPPER_IN3, STEPPER_IN4);
 
 void StepperInit() {
-  carousel.setMaxSpeed(1000.0);
-  carousel.setSpeed(500.0);
+  carousel.setMaxSpeed(2000.0);
+  carousel.setAcceleration(2000.0);
 }
 
 void GoToPlace() {
