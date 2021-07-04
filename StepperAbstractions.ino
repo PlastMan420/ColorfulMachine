18c91c1ffca1d874ae54008c6e093d17af3c7e21
PlastMAn420 <mostafa.alaa78@outlook.com>
2021-07-04 22:58:55 +0200

    [FEAT] determined how much the stepper should step per slot

--- StepperAbstractions.ino
+++ StepperAbstractions.ino
@@ -1,8 +1,9 @@
 #include <AccelStepper.h>
+#include <StreamUtils.h>
 #include "StepperAbstractions.h"
 
 const PROGMEM int STEPS = 850;
-
+int currentPosition = 0;
 // choose '4' in the first parameter for ULN2003AN controlling or similar.
 // IN1, pwmIN2, pwmIN3, pwmIN4.
 AccelStepper carousel(STEPPER_INTERFACE_TYPE, STEPPER_IN1, STEPPER_IN2, STEPPER_IN3, STEPPER_IN4);
@@ -12,10 +13,12 @@ void StepperInit() {
   carousel.setAcceleration(2000.0);
 }
 
-void GoToPlace() {
-  // .runToNewPosition() blocks the execusion thread until its finished.
-  carousel.runToNewPosition(0);
-  carousel.runToNewPosition(500);
-  carousel.runToNewPosition(100);
-  carousel.runToNewPosition(120);
+void GoToPlace(int hops) {
+  currentPosition += STEPS * hops;
+
+  carousel.runToNewPosition(currentPosition);
+  
 }
+
+// TODO
+// Stream stepper state into a file on sdcard
